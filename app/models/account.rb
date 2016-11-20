be9e6df4ea1db4051d198c230314a8415c3ba351
Ali <aqattan@yahoo.com>
2016-11-20 04:33:42 +0000

    updates to devise and models

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,6 @@
 class Account < ActiveRecord::Base
     belongs_to :user
     has_many :transactions
+    validates_presence_of :user_id
+    validates :balance, :numericality => { greater_than_or_equal_to: 0 }
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,4 @@
 class Transaction < ActiveRecord::Base
     belongs_to :account
+    validates_presence_of :account_id
 end
